dacb7506d3caf2485cfe23e45f0cb413009fe668
Ben Aston <ben.aston@justgiving.com>
2011-11-02 04:02:09 +0000

    HelpfulException is now a concrete type itself.

--- src/NHelpfulException/HelpfulException.cs
+++ src/NHelpfulException/HelpfulException.cs
@@ -6,14 +6,14 @@
     ///   When used as a base for custom exceptions, 
     ///   forces the supply of a message upon instantiation.
     /// </summary>
-    public abstract class HelpfulException : Exception
+    public class HelpfulException : Exception
     {
         public static readonly string ResolutionSuggestionListPrefix = "\r\nSuggestions:\r\n";
         public static readonly string ResolutionSuggestionPrefix = " - ";
         public static readonly string ResolutionSuggestionDelimiter = "\r\n";
         public static readonly string ResolutionSuggestionListSuffix = "\r\n";
 
-        protected HelpfulException(string problemDescription,
+        public HelpfulException(string problemDescription,
                                    string[] resolutionSuggestions = default(string[]),
                                    Exception innerException = default(Exception))
             : base(problemDescription + resolutionSuggestions.ToPrettyPrintList(ResolutionSuggestionListPrefix,
